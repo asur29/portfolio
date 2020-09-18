1b94b1b5bc11a737f950d446c0442f969604095e
sunny kumar <sunnymithlesh29@gmail.com>
2020-09-18 20:44:47 +0530

    last one

--- src/yourdata.js
+++ src/yourdata.js
@@ -2,16 +2,16 @@ export default
     {
         //(Please Do Not Remove The comma(,) after every variable)
         //Change The Website Template
-        name :'chetanverma.',
+        name :'sunny.',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
+                        'Hi 👋 You found me! I am Your sunny.',
                         //Line 2 For Header
                         'Digital designer ',
                         //Line 3 For Header
                         'from India'
     ],
         //Contact Email
-        contactEmail:'hello@chetanverma.com',
+        contactEmail:'sunnymithlesh29@gmail.com',
         // Add Your About Text Here
         abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
