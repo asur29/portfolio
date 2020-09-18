48fb36ac12e8d1215c5d2705bfc85c19360d98eb
sunny kumar <sunnymithlesh29@gmail.com>
2020-09-18 20:34:35 +0530

    my change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,6 @@ class Contact extends Component {
         </div>
         </Fade>
 
-            <span className='footer'>Made With ❤ by <a href="http://sunny.com/">sunny kumar</a></span>
         </div>);
     }
 }
